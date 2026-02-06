951613cb8ad215b422846e4f21e93ea2c18c50b4
striverdileep@gmail.com <striverdileep@gmail.com>
2026-02-06 10:58:52 +0530

    Created an endpoint POST /categories

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const pool = require("./db");
 
 const app = express();
+app.use(express.json());
 
 app.get("/", async (req, res) => {
   try {
     const conn = await pool.getConnection();
     console.log("Connected to DB");
     res.json({ message: "DB connected" }).status(200);
+    conn.release();
   } catch (err) {
     console.log("Failed to connect to DB");
     res.json({ message: "Couldn't connect to DB" }).status(404);
@@ -52,6 +54,38 @@ app.get("/categories/:id", async (req, res) => {
       .status(500)
       .json({ message: "Failed to fetch category from DB" });
   }
+});
+
+app.post("/categories", async (req, res) => {
+  const { name, description } = req.body;
+  if (!name || !description) {
+    return res
+      .status(400)
+      .json({ message: "Both name and Description are required" });
+  }
+  try {
+    const okPacket = await pool.query(
+      "INSERT INTO categories(name,description) VALUES(?,?)",
+      [name, description],
+    );
+    console.log(okPacket[0]);
+    if (okPacket[0].affectedRows > 0) {
+      return res
+        .status(200)
+        .json({ message: `Inserted id ${okPacket[0].insertId}` });
+    } else {
+      return res.status(400).json({ message: "Nothing was inserted" });
+    }
+  } catch (err) {
+    if (err && err.code === "ER_DUP_ENTRY") {
+      return res.status(409).json({
+        message: "Category with this name already exists",
+      });
+    }
+    console.log(err);
+    res.status(500).json({ message: "Failed to insert" });
+  }
+  return res.status(200).json({ message: "Success" });
 });
 
 app.listen(8000, () => {
