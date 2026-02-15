29a90365f725ad29f21c3a117a3e075115580551
Striver Dileep <striverdileep@gmail.com>
2026-02-15 15:01:31 +0530

    Added POST /categories endpoint

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 
 - GET /
 - GET /categories
+
+## Notes
+
+- INSERT, UPDATE, DELETE returns OKPACKET // OKPACKET[0]
+- GET return list res[0] --> object of rows
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ config();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", async (req, res) => {
   try {
     const conn = await pool.getConnection();
@@ -40,6 +42,49 @@ app.get("/categories", async (req, res) => {
   } catch (err) {
     console.log(err);
     return res.status(500).json({ error: "Error Fetching Categories" });
+  }
+});
+
+app.post("/categories", async (req, res) => {
+  let { name, description } = req.body;
+  if (!name || !description) {
+    return res.status(400).json({
+      error: "Both Name and Description are required to insert a category",
+    });
+  }
+  name = name.trim();
+  description = description.trim();
+  try {
+    const conn = await pool.getConnection();
+    if (conn) {
+      const result = await conn.query(
+        "INSERT INTO categories (name, description) VALUES(?,?)",
+        [name, description],
+      );
+      const okPacket = result[0];
+      conn.release();
+      if (okPacket?.affectedRows > 0) {
+        return res.status(201).json({
+          message: "Category Added Successfully",
+          id: okPacket.insertId,
+        });
+      }
+    } else {
+      console.log("Failed to connect to the Database");
+      return res
+        .status(500)
+        .json({ error: "Failed to connect to the Database" });
+    }
+  } catch (err) {
+    if (err.code === "ER_DUP_ENTRY") {
+      return res.status(400).json({ error: "Category Name Already Exists" });
+    }
+    console.log(err);
+    return res.status(500).json({ error: "Error Adding Category" });
+  } finally {
+    if (conn) {
+      conn.release();
+    }
   }
 });
 
